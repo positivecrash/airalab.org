cf0f649adc1d350415fa16efafbca645f517a3e7
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2020-04-30 15:04:58 +0400

    travis ci domain + in view check

--- src/pages/Service-blockchain-stamp.vue
+++ src/pages/Service-blockchain-stamp.vue
@@ -6,7 +6,8 @@
 	        <div class="texthighlight_yellow t-align-center"><h1>Сервис “Публичный блокчейн штамп” для данных на вашем производстве</h1></div>
        	</div>
 
-		<div class="shrimps" v-in-viewport.once>
+		<!-- <div class="shrimps" v-in-viewport.once> -->
+		<div class="shrimps">
 
 			<g-image class="shrimps__pack" alt="" src="@/assets/images/shrimp-pack.png"/>
 
@@ -79,7 +80,8 @@
 					</div>
 				</div>
 
-				<div class="stampTech__product" v-in-viewport.once>
+				<!-- <div class="stampTech__product" v-in-viewport.once> -->
+				<div class="stampTech__product">
 					<div class="stampTech__product__gray">
 
 						<template v-if="productNumber==1">
@@ -357,7 +359,7 @@
 			}
 		}
 
-		&.in-viewport{
+		// &.in-viewport{
 			.shrimps__pack{ animation: 1s cubic-bezier(.02,.52,.69,-0.3) 0.6s scaleIn forwards; }
 			.shrimps__ice_1{ animation: $speed_ice ease-in $start_ice fadeIn forwards; }
 			.shrimps__ice_2{ animation: $speed_ice ease-in $start_ice+$speed_ice/2 fadeIn forwards; }
@@ -406,7 +408,7 @@
 			.shrimps__pack_7 {
 				animation: $speed_qr ease-in $start_ice+($speed_ice/2)*17 fadeIn forwards, $speed_qr $func_packs $start_ice+($speed_ice/2)*17 pack7 forwards;
 			}
-		}
+		// }
 	}
 
 
@@ -520,7 +522,7 @@
 			$duration_product: 5s;
 			$delay_product: 1s;
 
-			&.in-viewport{
+			// &.in-viewport{
 				.stampTech__product__gray{
 					.item{
 						animation: moveProd $duration_product ease $delay_product infinite backwards, fadeIn 0.2s linear $delay_product forwards;
@@ -531,7 +533,7 @@
 						animation: moveProd $duration_product ease $duration_product/2 infinite backwards, fadeIn 0.2s linear $duration_product/2 forwards;
 					}
 				}
-			}
+			// }
 
 			@media screen and (max-width: 1000px){
 				display: none;
@@ -565,7 +567,7 @@ query {
 
 <script>
 
-import { enable, disable } from 'vue-in-viewport-directive'
+// import { enable, disable } from 'vue-in-viewport-directive'
 
 export default {
 
@@ -581,8 +583,8 @@ export default {
 	    }
   	},
   	mounted: function(){
-  		disable()
-		setTimeout(enable, 500)
+  // 		disable()
+		// setTimeout(enable, 500)
   	}
 }
 </script>
