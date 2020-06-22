caf94a090f181fd573483c831ae115c139d24325
Anastasiia Bakai (via Travis CI) <anastasiia.bakai@gmail.com>
2020-06-22 17:55:16 +0000

    Deploy positivecrash/airalab.org to github.com/positivecrash/airalab.org.git:gh-pages

--- assets/js/page--src--pages--index-vue.cc67ead1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{217:function(n,e,t){"use strict";t.r(e);t(18);var c={components:{Redirect:function(){return t.e(2).then(t.bind(null,220))}}},r=t(20),u=Object(r.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("Redirect")}),[],!1,null,null,null);e.default=u.exports}}]);
