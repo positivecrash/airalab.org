c9e9260d48c923eea32ee6d252a49061054eda63
positivecrash <positivecrash@users.noreply.github.com>
2021-12-07 10:44:57 +0000

    deploy: ff67404fb79f62cd4ab4828c17067d489ca95b43

---
+++ assets/js/page--src--pages--donate-done-vue.5c4f0417.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{248:function(n,e,t){"use strict";t.r(e);t(18);var c={components:{Redirect:function(){return t.e(2).then(t.bind(null,258))}}},r=t(20),u=Object(r.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("Redirect")}),[],!1,null,null,null);e.default=u.exports}}]);
